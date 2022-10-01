0b25cdf635131e1abd27012b89954e2e5d1c59f6
gabinhio91 <114810717+gabinhio91@users.noreply.github.com>
2022-10-01 16:03:51 +0300

    Add files via upload
    Prima lectie de C++

---
+++ c++ l1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  //  bool h=1;
+   // bool r=false;
+    //cout<<h<<" "<<r;
+    
+   // cout<<"marimea int in memorie este de:"<<sizeof(int)<<"bytes"<<endl;
+  //  cout<<"marimea double in memorie este de:"<<sizeof(double)<<"bytes"<<endl;
+    //cout<<"marimea float in memorie este de:"<<sizeof(float)<<"bytes"<<endl;
+    //cout<<"marimea long in memorie este de:"<<sizeof(long)<<"bytes"<<endl;
+    //cout<<"marimea bool in memorie este de:"<<sizeof(bool)<<"bytes"<<endl;
+//    cout<<"marimea unsigned int in memorie este de:"<<sizeof(unsigned int)<<"bytes"<<endl;
+
+  //  cout<<"salut"<<endl<<"mere\n"<<"pere";
+    
+    
+    char ccc= 'A';
+    int nr=(int)ccc+32;
+    cout<<(char)nr;
+
+
+    return 0;
+}

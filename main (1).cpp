c46349a6a520d99d737b3dcb1267112d5c4c42bc
gabinhio91 <114810717+gabinhio91@users.noreply.github.com>
2022-11-12 15:51:01 +0200

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main()
+{
+    
+    // int n,cmin,cmax;
+    // cin>>n;
+    // cmin=10;
+    // cmax=-1;
+    // while (n!=0){
+    //     if(n%10<=cmin)
+    //     cmin=n%10;
+    //     if(n%10>=cmax)
+    //     cmax=n%10;
+    //     n=n/10;
+    // }
+    // cout<<"cea mai mica cifra este "<<cmin<<" iar cea mai mare este "<<cmax;
+    
+    // int n,inv,n1;
+    // cin>>n;
+    // inv=0; n1=n;
+    // while(n1!=0){
+    //     inv=inv*10+n1%10;
+    //     n1=n1/10;
+    // }
+    //  cout<<"numarul initial este "<<n<<" iar inversul sau este "<<inv;
+    
+    
+    // int a,b,cmmdc;
+    // cin>>a>>b;
+    // while(b!=0){
+    //         int c=a%b;
+    //         a=b;
+    //         b=c;    
+    // }
+    // cout<<a;
+    
+  
+    // int a,b,ab;
+    // cin>>a>>b;
+    // ab=a*b;
+    // while(a!=b){
+    //     if(a>b)
+    //     a=a-b;
+    //     else
+    //     b=b-a;
+    // }
+    // cout<<"cmmdc ul numerelor este "<<a<<" iar cmmmc ul este "<<ab/a;
+  
+    
+    // int n,d,i,OK;
+    // cin>>n;
+    // OK=1;
+    // for(d=2; d<n; d++){
+    //     if(n%d==0){
+    //         OK=0;
+    //         break;
+    //     }
+    // }
+    // if(OK==1)
+    // cout<<n<<" este prim";
+    // else
+    // cout<<n<<" nu este prim";
+    
+    
+    // int n,x,d,i,OK;
+    // cin>>n;
+    // for(i=1; i<=n; i++)
+    // {
+    //     cin>>x;
+    //     OK=1;
+    //     for(d=2;d<x; d++){
+    //         if(x%d==0){
+    //             OK=0;
+    //             break;
+    //         }
+    //     }
+    //         if(OK==1)
+    // cout<<" este prim"<<endl;
+    // else
+    // cout<<" nu este prim"<<endl;
+        
+    // }
+    
+
+        
+    
+    
+    // int a,b,d,i,OK,c;
+    // cin>>a>>b;
+    // if(a>b){
+    //     c=a;
+    //     a=b;
+    //     b=c;
+    // }
+    
+    // for(i=a; i<=b; i++)
+    // {
+    //     OK=1;
+    //     for(d=2;d<i; d++){
+    //         if(i%d==0){
+    //             OK=0;
+    //             break;
+    //         }
+    //     }
+    //         if(OK==1)
+    // cout<<i<<" este prim"<<endl;
+    // else
+    // cout<<i<<" nu este prim"<<endl;
+        
+    // }
+    
+    
+    // int n,i,p;
+    // cin>>n;
+    // p=1;
+    // for(i=1; i<=n; i++){
+    //     p=p*i;
+    // }
+    // cout<<p;
+    
+    // int n,d,i,OK,y;
+    // cin>>n;
+    // for(i=1; i<=n; i++){
+    //     OK=1;
+    //     for(d=2; d<i; d++){
+    //         if(i%d==0)
+    //             OK=0;
+    //     }
+    //     if(OK==1)
+    //     y=i;
+    // }
+    // cout<<y;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}

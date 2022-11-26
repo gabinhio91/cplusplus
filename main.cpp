b85a0c26b0b0bc160de7a016d0fd35bad94691d4
gabinhio91 <114810717+gabinhio91@users.noreply.github.com>
2022-11-26 15:48:37 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+ 
+//   int n,i,d,x=0;
+//   cin>>n;
+//   for(i=1; i<=n; i++){
+//       for(d=1; d<=n; d++){
+//           if(d<=n-i)
+//           cout<<" ";
+//           else{
+//               x++;
+//           cout<<x<<" ";
+//           }
+//       }
+//       cout<<endl;
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ROMBUL ROMBUL
+
+//   int n,i,d;
+//   cin>>n;
+//   for(i=1; i<=n; i++){
+//       for(d=1; d<=n; d++){
+//           if(d<=n-i)
+//           cout<<" ";
+//           else
+//           cout<<"1 ";
+          
+//       }
+//       cout<<endl;
+//   }
+//   for(i=1; i<=n; i++){
+//       for(d=n; d>=1; d--){
+//           if(d<=n-i)
+//      cout<<"1 ";
+//           else
+//      cout<<" ";
+          
+//       }
+//       cout<<endl;
+//   }
+    
+    
+    
+    
+    //CLESPSIDRA CLEPSIDRA
+    
+//       int n,i,d;
+//         cin>>n;
+//       for(i=1; i<=n; i++){
+//       for(d=n; d>=0; d--){
+//           if(d<=n-i)
+//      cout<<"1 ";
+//           else
+//      cout<<" ";
+          
+//       }
+//       cout<<endl;
+//   }
+//       for(i=2; i<=n; i++){
+//       for(d=1; d<=n; d++){
+//           if(d<=n-i)
+//           cout<<" ";
+//           else
+//           cout<<" 1";
+          
+//       }
+//       cout<<endl;
+    
+//       }
+    
+    
+    
+    
+//       int n,i,d,x=1,y=0,z=0,s;
+//   cin>>n;
+//   for(i=1; i<=n; i++){
+//       x=1; y=0; z=0; 
+//       for(d=1; d<=n; d++){
+//           if(d<=n-i)
+//           cout<<" ";
+//           else{
+//           cout<<s<<" ";
+//           }
+//       }
+//       cout<<endl;
+//   }
+    
+    
+    
+    
+    
+//   TRIUNGIUL LUI PASCAL
+    
+//     int n,i,j,x=1,space;
+// 	cout<<"Enter the height of triangle "<<endl;
+// 	cin>>n;
+// 	for(i=0; i<n ;i++)
+// 	{
+// 	  for (space=1; space<=n-i; space++)
+// 	  {
+// 	  	cout<<" ";
+// 	  }	
+// 	  for (int j=0; j<=i; j++)
+// 	  {
+// 	  	if(j==0 || i==0)
+// 	  	   x=1;	
+// 	    else 
+// 	    	x=x*(i-j+1)/j;
+// 		cout << x<<" ";
+// 	  }
+// 	  cout<<endl;
+// 	}
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+return 0;
+}

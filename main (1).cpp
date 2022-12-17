a90845d4259ecc10100ecd2dbdd566b2dff2a2f6
gabinhio91 <114810717+gabinhio91@users.noreply.github.com>
2022-12-17 15:46:40 +0200

    Add files via upload

--- main (1).cpp
+++ main (1).cpp
@@ -1,137 +1,145 @@
 #include <iostream>
-#include <climits>
 using namespace std;
 int main()
 {
     
-    // int n,cmin,cmax;
+    // int a,i=1,cresc=1,b;
+    // cout<<"a = ";
+    // cin>>a;
+    // int n=7;
+    // while(i<n){
+    //     i++;
+    //     b=a;
+    //     cin>>a;
+    //     if(b>a)
+    //     cresc=0;
+    // }
+    // if(cresc==0)
+    // cout<<"nu e crescator";
+    // else
+    // cout<<"e crescator";
+    
+    
+    
+    
+    
+    //CITIRE VECTOR
+    //  for(int i=0; i<=n; i++){
+    //     cin>>b[i;]
+    // }
+    
+    
+    
+    
+    
+    // int a[100],b,c=1,n,i;
+    // cout<<"n= ";
     // cin>>n;
-    // cmin=10;
-    // cmax=-1;
-    // while (n!=0){
-    //     if(n%10<=cmin)
-    //     cmin=n%10;
-    //     if(n%10>=cmax)
-    //     cmax=n%10;
-    //     n=n/10;
+    // cin>>a[0];
+    // for(i=1; i<n; i++){
+    //     cin>>a[i];
+    //     if(a[i-1]>a[i])
+    //     c=0;
     // }
-    // cout<<"cea mai mica cifra este "<<cmin<<" iar cea mai mare este "<<cmax;
+    // if(c==1)
+    // cout<<"cresc";
+    // else
+    // cout<<"nu e cresc";
     
-    // int n,inv,n1;
+    
+    // int a[100],s,i,n;
+    // cout<<"n = ";
     // cin>>n;
-    // inv=0; n1=n;
-    // while(n1!=0){
-    //     inv=inv*10+n1%10;
-    //     n1=n1/10;
+    // cin>>a[0];
+    // s=a[0];
+    // for(i=1; i<n; i++){
+    //     cin>>a[i];
+    //     s=s+a[i];
     // }
-    //  cout<<"numarul initial este "<<n<<" iar inversul sau este "<<inv;
+    // cout<<"s = "<<s;
     
-    
-    // int a,b,cmmdc;
-    // cin>>a>>b;
-    // while(b!=0){
-    //         int c=a%b;
-    //         a=b;
-    //         b=c;    
+    // int a[100], nri=0, nrp=0, n, i;
+    // cout<<"n = ";
+    // cin>>n;
+    // for(i=0; i<n; i++){
+    //     cin>>a[i];
+    //     if(a[i]%2==0)
+    //     nrp++;
     // }
-    // cout<<a;
+    // nri=n-nrp;
+    // cout<<"numere pare = "<<nrp<<endl<<"numere impare = "<<nri;
     
-  
-    // int a,b,ab;
-    // cin>>a>>b;
-    // ab=a*b;
-    // while(a!=b){
-    //     if(a>b)
-    //     a=a-b;
+    
+    // int a[100], nr=0, s=0, n, i, d,ok;
+    // cout<<"n = ";
+    // cin>>n;
+    // for(i=0; i<n; i++){
+    //     cout<<"a = ";
+    //     cin>>a[i];
+    //     ok=1;
+    //     for(d=2; d<a[i]/2; d++){
+    //         if(a[i]%d==0)
+    //         ok=0;
+    //     }
+    //     if(ok==1)
+    //     cout<<"nr prim: "<<a[i]<<endl;
     //     else
-    //     b=b-a;
-    // }
-    // cout<<"cmmdc ul numerelor este "<<a<<" iar cmmmc ul este "<<ab/a;
-  
-    
-    // int n,d,i,OK;
-    // cin>>n;
-    // OK=1;
-    // for(d=2; d<n; d++){
-    //     if(n%d==0){
-    //         OK=0;
-    //         break;
+    //     cout<<"nr compus: "<<a[i]<<endl;
+    //     if(a[i]%2!=0){
+    //         s=s+a[i];
+    //         nr++;
     //     }
     // }
-    // if(OK==1)
-    // cout<<n<<" este prim";
-    // else
-    // cout<<n<<" nu este prim";
+    // cout<<endl<<"media artimetica a numerelor impare este: "<<s<<"/"<<nr<<"="<<s/nr;
     
-    
-    // int n,x,d,i,OK;
+    // int a[100], b[100], c[100], n, i, d, ok, x, y;
+    // cout<<"n = ";
     // cin>>n;
-    // for(i=1; i<=n; i++)
-    // {
-    //     cin>>x;
-    //     OK=1;
-    //     for(d=2;d<x; d++){
-    //         if(x%d==0){
-    //             OK=0;
-    //             break;
-    //         }
+    // x=0; y=0;
+    // for(i=0; i<n; i++){
+    //     cout<<"a = ";
+    //     cin>>a[i];
+    //     ok=1;
+    //     for(d=2; d<=a[i]/2; d++){
+    //         if(a[i]%d==0)
+    //         ok=0;
     //     }
-    //         if(OK==1)
-    // cout<<" este prim"<<endl;
-    // else
-    // cout<<" nu este prim"<<endl;
-        
+    //     if(ok==1){
+    //     x++;  
+    //      b[x]=a[i];
+    //     }
+    //     else{
+    //         y++;
+    //         c[y]=a[i];
+    //     }
     // }
-    
-
+    // cout<<"nr prime: ";
+    // for(i=x-1; i>=0; i--){
+    //     cout<<b[x-i]<<" ";
+    // }
+    // cout<<"nr compuse: ";
+    // for(i=y-1; i>=0; i--){
+    //     cout<<c[y-i]<<" ";
+    // }
         
     
     
-    // int a,b,d,i,OK,c;
-    // cin>>a>>b;
-    // if(a>b){
-    //     c=a;
-    //     a=b;
-    //     b=c;
-    // }
-    
-    // for(i=a; i<=b; i++)
-    // {
-    //     OK=1;
-    //     for(d=2;d<i; d++){
-    //         if(i%d==0){
-    //             OK=0;
-    //             break;
-    //         }
-    //     }
-    //         if(OK==1)
-    // cout<<i<<" este prim"<<endl;
-    // else
-    // cout<<i<<" nu este prim"<<endl;
-        
-    // }
     
     
-    // int n,i,p;
-    // cin>>n;
-    // p=1;
-    // for(i=1; i<=n; i++){
-    //     p=p*i;
-    // }
-    // cout<<p;
     
-    // int n,d,i,OK,y;
-    // cin>>n;
-    // for(i=1; i<=n; i++){
-    //     OK=1;
-    //     for(d=2; d<i; d++){
-    //         if(i%d==0)
-    //             OK=0;
-    //     }
-    //     if(OK==1)
-    //     y=i;
-    // }
-    // cout<<y;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
